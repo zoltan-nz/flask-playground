efc92b731caa921959423267812388cc5b4f2627
Zoltan <zdebre@gmail.com>
2019-04-19 22:16:00 +1200

    Fix gitlab configuration

--- alpine-docker-setup.sh
+++ alpine-docker-setup.sh
@@ -3,6 +3,7 @@
 apk add --no-cache --update python3-dev gcc build-base
 python -V
 pip install pipenv
+pipenv --rm || true
 pipenv run setup
 pipenv run test
 pipenv run cov
@@ -10,4 +11,5 @@ pipenv run cov_report
 
 python setup.py bdist_wheel
 pip install dist/*
+pipenv run env
 pipenv run app
